672a2add08554e837f274a3d1d4bf5d287d2f2ae
rodrigo <rodrigo@rodrigo-H61M-D2P-B3.(none)>
2013-04-17 14:47:59 -0300

    tests

--- test.py
+++ test.py
@@ -1,36 +1,8 @@
-import Image
-import time
-import src.Algorithm.Singularity as Singularity
-import src.Algorithm.Sandbox as Sandbox
-import src.Algorithm.MFS as MFS
+import tests/test_MFS
+except ImportError:
+    print "Error: the module tests/tests_MFS is missing"
 
-filename = 'images/baguette2.tif'
+import tests/test_classifier
+except ImportError:
+    print "Error: the module tests/tests_classifier is missing"
 
-i = Singularity.Singularity(20)
-
-print "Calculating Singularity Multifractal Spectrum..."
-t =  time.clock()
-fds = i.getFDs(filename)
-t =  time.clock()-t
-print "Time Singularity: ", t
-print fds
-
-i = Sandbox.Sandbox(14)
-i.setDef(40,1.15)
-
-print "Calculating Sandbox Multifractal Spectrum..."
-t =  time.clock()
-fds2 = i.getFDs(filename)
-t =  time.clock()-t
-print "Time Sandbox: ", t
-print fds2
-
-i = MFS.MFS()
-i.setDef(1,20,3,True)
-
-print "Calculating MFS Multifractal Spectrum..."
-t =  time.clock()
-fds3 = i.getFDs(filename)
-t =  time.clock()-t
-print "Time MFS: ", t
-print fds3
