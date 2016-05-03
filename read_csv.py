5dd2ef64087aadfa6eed5e29e6208ef50d845e68
Rodrigo Baravalle <rbaravalle@gmail.com>
2016-05-03 17:40:49 -0300

    Added reader from csv file

---
+++ read_csv.py
@@ -0,0 +1,27 @@
+import sys
+import csv
+import numpy as np
+
+f = open(sys.argv[1], 'rb') # opens the csv file
+
+from numpy import recfromcsv
+my_data = recfromcsv(sys.argv[1], delimiter=',')
+
+
+print my_data
+
+np.save("bioAsset_meta.npy", my_data)
+
+exit()
+
+try:
+    reader = csv.reader(f)  # creates the reader object
+    for row in reader:   # iterates the rows of the file in orders
+        print row    # prints each row
+        np.vstack((data, row))
+
+finally:
+    f.close()      # closing
+
+
+print data
