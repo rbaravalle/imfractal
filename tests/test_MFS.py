46fe1a1b9746f679d0880dc7731ab903c24df991
Rodrigo Baravalle <rbaravalle@gmail.com>
2016-08-02 17:01:08 -0300

    Cleaned test

--- tests/test_MFS.py
+++ tests/test_MFS.py
@@ -28,22 +28,22 @@ from imfractal import *
 import time
 
 def do_test():
-    path = '/almacen/members.imaglabs.org/felix.thomsen/Rodrigo/BioAsset/mats/'
-    filename = path + 'BA01_120_1Slices.mat'
-    file_mask = path + 'BA01_120_1Mask.mat'
-    i = MFS_3D()
-    i.setDef(1, 20, 3, filename, file_mask)
-    print "Calculating 3D MFS Multifractal Spectrum (Holder)..."
-    t = time.clock()
-    fds3 = i.getFDs()
-    t = time.clock() - t
-    print "Time 3D MFS: ", t
-    print fds3
+    #path = '/almacen/members.imaglabs.org/felix.thomsen/Rodrigo/BioAsset/mats/'
+    #filename = path + 'BA01_120_1Slices.mat'
+    #file_mask = path + 'BA01_120_1Mask.mat'
+    #i = MFS_3D()
+    #i.setDef(1, 20, 3, filename, file_mask)
+    #print "Calculating 3D MFS Multifractal Spectrum (Holder)..."
+    #t = time.clock()
+    #fds3 = i.getFDs()
+    #t = time.clock() - t
+    #print "Time 3D MFS: ", t
+    #print fds3
 
 
     filename = 'images/baguette2.tif'#fractal20Bread.png'
     i = MFS()
-    i.setDef(1,20,3,True)
+    i.setDef(1,20,3)
     print "Calculating MFS Multifractal Spectrum..."
     t =  time.clock()
     fds3 = i.getFDs(filename)
@@ -53,15 +53,15 @@ def do_test():
 
     i = Singularity(20)
 
-    print "Calculating Singularity Multifractal Spectrum..."
-    t =  time.clock()
-    fds = i.getFDs(filename)
-    t =  time.clock()-t
-    print "Time Singularity: ", t
-    print fds
+#    print "Calculating Singularity Multifractal Spectrum..."
+    #t =  time.clock()
+    #fds = i.getFDs(filename)
+    #t =  time.clock()-t
+    #print "Time Singularity: ", t
+    #print fds
 
     i = Sandbox(14)
-    i.setDef(40,1.15)
+    i.setDef(40,1.15, True)
 
     print "Calculating Sandbox Multifractal Spectrum..."
     t =  time.clock()
@@ -70,12 +70,3 @@ def do_test():
     print "Time Sandbox: ", t
     print fds2
 
-    i = MFS()
-    i.setDef(1,20,3,True)
-
-    print "Calculating MFS Multifractal Spectrum..."
-    t =  time.clock()
-    fds3 = i.getFDs(filename)
-    t =  time.clock()-t
-    print "Time MFS: ", t
-    print fds3
