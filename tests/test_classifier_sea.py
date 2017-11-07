e771148a2c818e2d8b71b84f31d2062f98e08fb6
Rodrigo Baravalle <rbaravalle@gmail.com>
2017-11-07 12:47:02 -0300

    Taking resolutions from the images

--- tests/test_classifier_sea.py
+++ tests/test_classifier_sea.py
@@ -45,6 +45,9 @@ from scipy import misc
 from scipy.stats import mode
 
 model_directory = "model/"
+path_images = 'images/sea/first/'
+data_path = "exps/data/"
+
 filename_model_RF_tt=model_directory+"RF_tt_{}.pkl"
 filename_model_SVC_tt=model_directory+"SVC_tt_{}.pkl"
 
@@ -54,6 +57,18 @@ dolphin_label=2
 dDFs  = 5
 
 YIQ = False
+
+# detect resolutions
+dirs = os.listdir(path_images)
+resolutions = []
+
+for d in dirs:
+    if 'sea' in d:
+        _, resx = d.split('sea')
+        res, _ = resx.split('x')
+        resolutions.append(res)
+
+resolutions.sort(reverse = True)
 
 def transform_and_eq_hist(im):
 
@@ -72,8 +87,8 @@ def compute_MFS(path_sea, path_dolphin):
     cant_sea = len(dir_sea)
     cant_dolphin = len(dir_dolphin)
 
-    seatrain = np.zeros((cant_sea, dDFs)).astype(np.float32)
-    dolphintrain = np.zeros((cant_dolphin, dDFs)).astype(np.float32)
+    seatrain_i = np.zeros((cant_sea, dDFs)).astype(np.float32)
+    dolphintrain_i = np.zeros((cant_dolphin, dDFs)).astype(np.float32)
 
     ins = MFS()
     ins.setDef(1,dDFs,3)
@@ -85,9 +100,9 @@ def compute_MFS(path_sea, path_dolphin):
             im = Image.open(filename)
             im_eq = transform_and_eq_hist(im)
             
-            seatrain[i] = ins.getFDs('', im_eq)
+            seatrain_i[i] = ins.getFDs('', im_eq)
         else:
-            seatrain[i] = ins.getFDs(filename)
+            seatrain_i[i] = ins.getFDs(filename)
 
     for i in range(cant_dolphin):
         filename = path_dolphin + dir_dolphin[i]
@@ -96,88 +111,43 @@ def compute_MFS(path_sea, path_dolphin):
             im = Image.open(filename)
             im_eq = transform_and_eq_hist(im)
             
-            seatrain[i] = ins.getFDs('', im_eq)
+            seatrain_i[i] = ins.getFDs('', im_eq)
         else:
-            dolphintrain[i] = ins.getFDs(filename)
+            dolphintrain_i[i] = ins.getFDs(filename)
 
+    return seatrain_i, dolphintrain_i
+
+
+
+
+def prepare_dataset(seatrain, dolphintrain):
+    i = 0 # FIX ME
+    for r in resolutions:
+        if os.path.isfile(data_path+"seatrain"+str(r)+".npy")  \
+        and os.path.isfile(data_path+"dolphintrain"+str(r)+".npy"):
+            print i, len(seatrain)
+            seatrain[i] = np.load(data_path+"seatrain"+str(r)+".npy")
+            dolphintrain[i] = np.load(data_path+"dolphintrain"+str(r)+".npy")
+
+        else:
+            print "Computing MFSs at Resolution "+str(r)+"..."
+            
+            path_sea = path_images+'sample-point-sea'+str(r)+'x'+str(r)+'/'
+            path_dolphin = path_images+'sample-point-dolphin'+str(r)+'x'+str(r)+'/'
+              
+            seatrain[i], dolphintrain[i] = compute_MFS(path_sea, path_dolphin)
+
+            np.save(data_path+"seatrain"+r+".npy", seatrain[i])
+            np.save(data_path+"dolphintrain"+r+".npy", dolphintrain[i])
+
+        i+=1
+
+    
     return seatrain, dolphintrain
-
-computeMFS = False
-seatrain86 = ""
-seatrain64 = ""
-seatrain32 = ""
-seatrain16 = ""
-
-dolphintrain86 = ""
-dolphintrain64 = ""
-dolphintrain32 = ""
-dolphintrain16 = ""
-
-data_path = "exps/data/"
-
-if os.path.isfile(data_path+"seatrain86.npy")  \
-and os.path.isfile(data_path+"dolphintrain86.npy"):
-    seatrain86 = np.load(data_path+"seatrain86.npy")
-    dolphintrain86 = np.load(data_path+"dolphintrain86.npy")
-
-else:
-    print "Computing MFSs at Resolution 86..."
-    
-    path_sea = 'images/sea/sample-point-sea86x86/'
-    path_dolphin = 'images/sea/sample-point-dolphin86x86/'
-      
-    seatrain86, dolphintrain86 = compute_MFS(path_sea, path_dolphin)
-
-    np.save(data_path+"seatrain86.npy", seatrain86)
-    np.save(data_path+"dolphintrain86.npy", dolphintrain86)
-
-if os.path.isfile(data_path+"seatrain64.npy") \
-and os.path.isfile(data_path+"dolphintrain64.npy"):
-    seatrain64 = np.load(data_path+"seatrain64.npy")
-    dolphintrain64 = np.load(data_path+"dolphintrain64.npy")
-else:
-    print "Computing MFSs at Resolution 64..."
-    
-    path_sea = 'images/sea/sample-point-sea64x64/'
-    path_dolphin = 'images/sea/sample-point-dolphin64x64/'
-    
-    seatrain64, dolphintrain64 = compute_MFS(path_sea, path_dolphin)
-    
-    np.save(data_path+"seatrain64.npy", seatrain64)
-    np.save(data_path+"dolphintrain64.npy", dolphintrain64)
-
-if os.path.isfile(data_path+"seatrain32.npy")  \
-and os.path.isfile(data_path+"dolphintrain32.npy"):
-    seatrain32 = np.load(data_path+"seatrain32.npy")
-    dolphintrain32 = np.load(data_path+"dolphintrain32.npy")
-else:
-    print "Computing MFSs at Resolution 32..."
-    
-    path_sea = 'images/sea/sample-point-sea32x32/'
-    path_dolphin = 'images/sea/sample-point-dolphin32x32/'
-    
-    seatrain32, dolphintrain32 = compute_MFS(path_sea, path_dolphin)
-    
-    np.save(data_path+"seatrain32.npy", seatrain32)
-    np.save(data_path+"dolphintrain32.npy", dolphintrain32)
-
-if os.path.isfile(data_path+"seatrain16.npy") \
-and os.path.isfile(data_path+"dolphintrain16.npy"):
-    seatrain16 = np.load(data_path+"seatrain16.npy")
-    dolphintrain16 = np.load(data_path+"dolphintrain16.npy")
-else:
-    print "Computing MFSs at Resolution 16..."
-    
-    path_sea = 'images/sea/sample-point-sea16x16/'
-    path_dolphin = 'images/sea/sample-point-dolphin16x16/'
-    
-    seatrain16, dolphintrain16 = compute_MFS(path_sea, path_dolphin)
-    
-    np.save(data_path+"seatrain16.npy", seatrain16)
-    np.save(data_path+"dolphintrain16.npy", dolphintrain16)
+     
 
 
-def test_model_amount(train_size, seatrain, dolphintrain, resol):
+def test_model_amount(train_size, seatrain_i, dolphintrain_i, resol):
 
     if train_size == 0:
         return
@@ -185,13 +155,13 @@ def test_model_amount(train_size, seatrain, dolphintrain, resol):
     cfr = RandomForestClassifier(n_estimators=100)
     cfr2 = svm.LinearSVC(C=1.0)
 
-    data = np.vstack((seatrain, dolphintrain))
-    labels_sea = np.zeros(len(seatrain)) + sea_label
-    labels_dolphin = np.zeros(len(dolphintrain)) + dolphin_label
+    data = np.vstack((seatrain_i, dolphintrain_i))
+    labels_sea = np.zeros(len(seatrain_i)) + sea_label
+    labels_dolphin = np.zeros(len(dolphintrain_i)) + dolphin_label
     labels = np.hstack((labels_sea, labels_dolphin))
     
-    X_train_sea, X_test_sea, y_train_sea, y_test_sea = train_test_split(seatrain, labels_sea, train_size=min(train_size,len(seatrain)), random_state=0)
-    X_train_do, X_test_do, y_train_do, y_test_do = train_test_split(dolphintrain, labels_dolphin, train_size=min(train_size,len(dolphintrain)), random_state=0)
+    X_train_sea, X_test_sea, y_train_sea, y_test_sea = train_test_split(seatrain_i, labels_sea, train_size=min(train_size,len(seatrain_i)), random_state=0)
+    X_train_do, X_test_do, y_train_do, y_test_do = train_test_split(dolphintrain_i, labels_dolphin, train_size=min(train_size,len(dolphintrain_i)), random_state=0)
 
     X_train = np.vstack((X_train_sea, X_train_do))
     X_test = np.vstack((X_test_sea, X_test_do))
@@ -211,30 +181,30 @@ def test_model_amount(train_size, seatrain, dolphintrain, resol):
     joblib.dump(clf2, outdir_clf2)
     
     print "RF, SVM: " + str( round(clf.score(X_test, y_test), 4) ) + " " + str( round(clf2.score(X_test, y_test), 4) )
-    
+        
 
 
 
-def test_path(amount, seatrain, dolphintrain, resol):
+def test_path(amount, seatrain_i, dolphintrain_i, resol):
 
-    cant_sea = len(seatrain)
-    cant_dolphin = len(dolphintrain)
+    cant_sea = len(seatrain_i)
+    cant_dolphin = len(dolphintrain_i)
 
     if amount > 0:
         cant_sea = min(amount, cant_sea)
         cant_dolphin = min(amount, cant_dolphin)
 
-    seatrain = seatrain[:cant_sea]
-    dolphintrain = dolphintrain[:cant_dolphin]
+    seatrain_i = seatrain_i[:cant_sea]
+    dolphintrain_i = dolphintrain_i[:cant_dolphin]
 
 
     cfr = RandomForestClassifier(n_estimators=100)
     cfr2 = svm.LinearSVC(C=1.0)
 
-    data = np.vstack((seatrain, dolphintrain))
+    data = np.vstack((seatrain_i, dolphintrain_i))
 
-    labels_sea = np.zeros(len(seatrain)) + 1
-    labels_dolphin = np.zeros(len(dolphintrain)) + 2
+    labels_sea = np.zeros(len(seatrain_i)) + 1
+    labels_dolphin = np.zeros(len(dolphintrain_i)) + 2
     labels = np.hstack((labels_sea, labels_dolphin))
 
     scores_rf = cross_validation.cross_val_score(cfr, data, labels, cv=4)
@@ -242,46 +212,41 @@ def test_path(amount, seatrain, dolphintrain, resol):
     
     print "RF, SVM: " + str( round(np.array(scores_rf).mean(),4) ) + " " + str( round(np.array(scores_svm).mean(),4) )
 
-def test_all_resolutions(amount, func):
-    print "86x86"
-    func(amount, seatrain86, dolphintrain86, 86)
-    
-    print "64x64"
-    func(amount, seatrain64, dolphintrain64, 64)
- 
-    print "32x32"
-    func(amount, seatrain32, dolphintrain32, 32)
-     
-    print "16x16"
-    func(amount, seatrain16, dolphintrain16, 16)
+def test_all_resolutions(amount, func, seatrain, dolphintrain):
+    i = 0
+    for r in resolutions:
+        print r, 'x', r
+        func(amount, seatrain[i], dolphintrain[i], r)
+        i+=1
 
-def test_train_predict():
+def test_train_predict(seatrain, dolphintrain):
     print " "
     print "########### Training-predicting test"
 
     for a in range(5,60,5):
         print ""
         print a, " samples"
-        test_all_resolutions(a, test_model_amount)
+        test_all_resolutions(a, test_model_amount, seatrain, dolphintrain)
 
 
-def test_cross_val():
+def test_cross_val(seatrain, dolphintrain):
     print " "
     print "########### Results with 4-cross validation"
 
-#    for a in range(0,60,5):
     for a in range(0,40,5):
         print ""
         if a == 0:
             print "All samples"
         else:
             print a, " samples"
-        test_all_resolutions(a, test_path)
+        test_all_resolutions(a, test_path, seatrain, dolphintrain)
   
     
 def do_test():
+    seatrain = [[] for i in range(len(resolutions))]
+    dolphintrain = [[] for i in range(len(resolutions))]
 
-    test_cross_val()
-    test_train_predict()
+    seatrain, dolphintrain = prepare_dataset(seatrain, dolphintrain)
+    test_cross_val(seatrain, dolphintrain)
+    test_train_predict(seatrain, dolphintrain)
     
-#do_test()
