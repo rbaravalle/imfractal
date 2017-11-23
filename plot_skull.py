78c12c43bd22c74e447fb432d37c5f81f037c6b1
Baravalle, Rodrigo <rbaravalle@gmail.com>
2017-11-23 15:24:39 -0300

    Added cross validation and parameters

--- plot_skull.py
+++ plot_skull.py
@@ -1,13 +1,57 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from sklearn.ensemble import RandomForestClassifier
+from sklearn import cross_validation
+from sklearn import svm
+import argparse
+
+# Nc-cross validation type
+# FIXME if you want a parameter
+Nc = 4
+
+def cross_v(mfss):
+
+    labels = [0 for j in range(len(mfss[0]))]
+    data = mfss[0]
+    
+    for i in range(1,mfss.shape[0]):
+        data = np.vstack((data, mfss[i]))
+        labels_i = [i for j in range(len(mfss[i]))]
+        labels.extend(labels_i)
+
+    cfr = RandomForestClassifier(n_estimators=100)
+    cfr2 = svm.LinearSVC(C=1.0)
+
+    labels = np.array(labels)
+
+    scores_rf = cross_validation.cross_val_score(cfr, data, labels, cv=Nc)
+    scores_svm = cross_validation.cross_val_score(cfr2, data, labels, cv=Nc)
+    
+    print Nc, "- Cross Validation scores"
+    print "RForest, SVM: " + str( round(np.array(scores_rf).mean(),Nc) ) + " " + str( round(np.array(scores_svm).mean(),Nc) )
 
 def main():
-    mfss = np.load('suturas.npy')
+    parser = argparse.ArgumentParser(description='Test '+str(Nc)+'-cross validation of mfss classes')
+    parser.add_argument("-npy", dest="npy", type=str, required=True, nargs=1, help="npy you want to test")
+    parser.add_argument("-plot", dest="plot", type=str, required=False, nargs=1, help="Plot or not")
+
+    args = parser.parse_args()
+
+    mfss = np.load(args.npy[0])
+    cross_v(mfss)
+
+    if args.plot:
+        plot(args.npy[0])
+
+def plot(npy):
+    mfss = np.load(npy)
     for i in range(mfss.shape[0]):
         for j in range(len(mfss[i])):
-            x = mfss[i][j]
+            x = np.array(mfss[i][j])
+            print x
             plt.plot(x, color='C'+str(i%10))
-        plt.show()
+    plt.title('All samples')
+    plt.show()
 
 if __name__ == '__main__':
     main()
